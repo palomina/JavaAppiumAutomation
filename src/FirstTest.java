199c7f9c8dbdfc964ca8d7c5589f28210dd9e2c8
MorozovaO <palomina.1983@gmail.com>
2021-06-09 16:06:52 +0300

    Тест, который проверяет, что поле ввода для поиска статьи содержит текст

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,15 +1,24 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
 public class FirstTest {
 
     private AppiumDriver driver;
+
+    private By buttonSkip = By.id("org.wikipedia:id/fragment_onboarding_skip_button");
+    private By fieldsSearch = By.xpath("//*[@resource-id='org.wikipedia:id/search_container']//android.widget.TextView");
+    private By inputSearch = By.id("org.wikipedia:id/search_src_text");
 
     @Before
     public void setUp() throws Exception
@@ -38,5 +47,41 @@ public class FirstTest {
     {
         System.out.println("First test run");
     }
+
+    public void waitElementAndClick(By by, String errorMessage, int timeout) {
+        new WebDriverWait(driver, timeout)
+                .withMessage(errorMessage)
+                .until(ExpectedConditions.presenceOfElementLocated(by))
+                .click();
+    }
+
+    public void assertElementHasText(By by, String text, String errorMessage) {
+        WebElement element = driver.findElement(by);
+        Assert.assertTrue(errorMessage, element.getText().contains(text));
+    }
+
+
+    @Test
+    public void checkText() {
+        waitElementAndClick(
+                buttonSkip,
+                "Button `Skip` not found",
+                20
+        );
+
+        waitElementAndClick(
+                fieldsSearch,
+                "Can't click on search panel",
+                20
+        );
+
+        assertElementHasText(
+                inputSearch,
+                "Search Wikipedia",
+                "Unexpected text"
+        );
+    }
+
+
 
 }
