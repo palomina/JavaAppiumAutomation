313d26e83b6d99ad497ce53159c66357d95a8368
MorozovaO <palomina.1983@gmail.com>
2021-06-14 16:14:03 +0300

    Ex5: Тест: сохранение двух статей

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,4 +1,5 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
@@ -23,7 +24,16 @@ public class FirstTest {
     private By inputSearch = By.id("org.wikipedia:id/search_src_text");
     private By panelSearchResult = By.id("org.wikipedia:id/search_results_list");
     private By itemSearchResult = By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']//android.view.ViewGroup//*[@resource-id='org.wikipedia:id/page_list_item_title']");
+    private By itemSearchFirstResult = By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']//android.view.ViewGroup[1]//*[@resource-id='org.wikipedia:id/page_list_item_title']");
+    private By itemSearchSecondResult = By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']//android.view.ViewGroup[2]//*[@resource-id='org.wikipedia:id/page_list_item_title']");
     private By panelSearchEmptyResult = By.id("org.wikipedia:id/search_empty_container");
+    private By buttonBack = By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']");
+
+    private By buttonSaveToList = By.id("org.wikipedia:id/article_menu_bookmark");
+    private By buttonAddToList = By.id("org.wikipedia:id/snackbar_action");
+    private By inputListName = By.id("org.wikipedia:id/text_input");
+    private By buttonOk = By.xpath("//*[@text='OK']");
+    private By buttonViewList = By.xpath("//*[@text='VIEW LIST']");
 
     @Before
     public void setUp() throws Exception
@@ -67,6 +77,19 @@ public class FirstTest {
                 .sendKeys(text);
     }
 
+    public WebElement waitElementPresent(By by, String errorMessage, int timeout) {
+       return new WebDriverWait(driver, timeout)
+                .withMessage(errorMessage)
+                .until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    public String waitElementAndGetAttribute(By by, String attributeName, String errorMessage, int timeout) {
+       return new WebDriverWait(driver, timeout)
+                .withMessage(errorMessage)
+                .until(ExpectedConditions.presenceOfElementLocated(by))
+               .getAttribute(attributeName);
+    }
+
     public void assertElementHasText(By by, String text, String errorMessage) {
         WebElement element = driver.findElement(by);
         Assert.assertTrue(errorMessage, element.getText().contains(text));
@@ -77,17 +100,30 @@ public class FirstTest {
         Assert.assertTrue(errorMessage, elements.size() > 0);
     }
 
-    public void assertElementPresent(By by, String errorMessage, int timeout) {
-        new WebDriverWait(driver, timeout)
-                .withMessage(errorMessage)
-                .until(ExpectedConditions.presenceOfElementLocated(by));
-    }
 
     public void assertElementsHasText(By by, String text, String errorMessage) {
         List<WebElement> elements = driver.findElements(by);
         for (WebElement element : elements) {
             Assert.assertTrue(errorMessage, element.getText().contains(text));
         }
+    }
+
+    protected void swipeElementToLeft(By by, String errorMessage) {
+        WebElement element = waitElementPresent(by, errorMessage, 10);
+
+        int leftX = element.getLocation().getX();
+        int rightX = leftX + element.getSize().getWidth();
+        int upperY = element.getLocation().getY();
+        int lowerY = upperY + element.getSize().getHeight();
+        int middleY = (upperY + lowerY) / 2;
+
+        TouchAction action = new TouchAction(driver);
+        action
+                .press(rightX, middleY)
+                .waitAction(300)
+                .moveTo(leftX, middleY)
+                .release()
+                .perform();
     }
 
 
@@ -136,13 +172,13 @@ public class FirstTest {
                 20
         );
 
-        assertElementPresent(
+        waitElementPresent(
                 panelSearchResult,
                 "Panel of search result is not found",
                 20
         );
 
-        assertElementPresent(
+        waitElementPresent(
                 itemSearchResult,
                 "Items of search result are not found",
                 20
@@ -159,7 +195,7 @@ public class FirstTest {
                 20
         );
 
-        assertElementPresent(
+        waitElementPresent(
                 panelSearchEmptyResult,
                 "Panel of empty result is not found",
                 20
@@ -191,13 +227,13 @@ public class FirstTest {
                 20
         );
 
-        assertElementPresent(
+        waitElementPresent(
                 panelSearchResult,
                 "Panel of search result is not found",
                 20
         );
 
-        assertElementPresent(
+        waitElementPresent(
                 itemSearchResult,
                 "Items of search result are not found",
                 20
@@ -215,11 +251,154 @@ public class FirstTest {
                 20
         );
 
-        assertElementPresent(
+        waitElementPresent(
                 panelSearchEmptyResult,
                 "Panel of empty result is not found",
                 20
         );
 
     }
+
+
+    @Test
+    public void saveTwoArticle() {
+        String word = "Java";
+        String listName = "List1";
+        String firstArticleName;
+        String secondArticleName;
+        String titleArticleFromList;
+
+
+        waitElementAndClick(
+                buttonSkip,
+                "Button `Skip` not found",
+                20
+        );
+
+        waitElementAndClick(
+                fieldsSearch,
+                "Can't click to the search panel",
+                20
+        );
+
+        waitElementAndSendKeys(
+                inputSearch,
+                word,
+                "Can't set the search word",
+                20
+        );
+
+        waitElementPresent(
+                panelSearchResult,
+                "Panel of search result is not found",
+                20
+        );
+
+        firstArticleName = waitElementAndGetAttribute(
+                itemSearchFirstResult,
+                "text",
+                "The first item of search result is not found",
+                20
+        );
+
+        waitElementAndClick(
+                itemSearchFirstResult,
+                "Can't click to the first article",
+                20
+        );
+
+        waitElementAndClick(
+                buttonSaveToList,
+                "Can't click to the button `Save to List`",
+                20
+        );
+
+        waitElementAndClick(
+                buttonAddToList,
+                "Can't click to the button `Add to List`",
+                20
+        );
+
+        waitElementAndSendKeys(
+                inputListName,
+                listName,
+                "Can't set the list's name",
+                20
+        );
+
+        waitElementAndClick(
+                buttonOk,
+                "Can't click to the button `Ok`",
+                20
+        );
+
+        waitElementAndClick(
+                buttonBack,
+                "Can't click to the button `Back`",
+                20
+        );
+
+        secondArticleName = waitElementAndGetAttribute(
+                itemSearchSecondResult,
+                "text",
+                "The second item of search result is not found",
+                20
+        );
+
+        waitElementAndClick(
+                itemSearchSecondResult,
+                "Can't click to the second article",
+                20
+        );
+
+        waitElementAndClick(
+                buttonSaveToList,
+                "Can't click to button `Save to List`",
+                20
+        );
+
+        waitElementAndClick(
+                buttonAddToList,
+                "Can't click to button `Add to List`",
+                20
+        );
+
+        waitElementAndClick(
+                By.xpath("//android.view.ViewGroup//android.widget.TextView[@text='"+listName+"']"),
+                "Can't click to button `Add to List`",
+                20
+        );
+
+        waitElementAndClick(
+                buttonViewList,
+                "Can't click to button `View List`",
+                20
+        );
+
+        waitElementPresent(
+                By.xpath("//*[@text='"+firstArticleName+"']"),
+                "First article is not found in list",
+                10
+        );
+
+        swipeElementToLeft(
+                By.xpath("//*[@text='"+firstArticleName+"']"),
+                "Can't swipe"
+        );
+        waitElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title' and @text='"+secondArticleName+"']"),
+                "Second article is not found in the list",
+                10
+        );
+
+        titleArticleFromList = waitElementAndGetAttribute(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title' and @text='"+secondArticleName+"']"),
+                "text",
+                "The title of second article in the list is not present",
+                10
+        );
+
+        Assert.assertEquals("The name of the article is not the same is expected!", secondArticleName, titleArticleFromList);
+    }
+
 }
